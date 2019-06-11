aef540e259f1d01d0b568c91ea0780a662297c3b
Juan Jesús <j.saes@preving.com>
2019-06-11 12:36:42 +0200

    Solucionando errores

--- back/src/main/java/com/curso/spring/rest/controllers/ClienteRestController.java
+++ back/src/main/java/com/curso/spring/rest/controllers/ClienteRestController.java
@@ -12,13 +12,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:4200"}, methods = RequestMethod.*)
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
