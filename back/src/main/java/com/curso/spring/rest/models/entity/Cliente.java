1d0494b08f0b287d4678f3852dea9a8b6c518e52
Jimmyfk <jjsaesd01@gmail.com>
2019-06-19 22:30:34 +0200

    Import eliminado

--- back/src/main/java/com/curso/spring/rest/models/entity/Cliente.java
+++ back/src/main/java/com/curso/spring/rest/models/entity/Cliente.java
@@ -13,7 +13,6 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 
