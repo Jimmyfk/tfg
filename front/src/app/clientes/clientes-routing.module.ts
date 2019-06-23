808bb399b5bae9576e85062c458ca8928a97c3a8
Jimmyfk <jjsaesd01@gmail.com>
2019-06-23 13:20:27 +0200

    Clientes routing terminado

--- front/src/app/clientes/clientes-routing.module.ts
+++ front/src/app/clientes/clientes-routing.module.ts
@@ -6,10 +6,7 @@ const routes: Routes = [
     path: '',
     children: [
       { path: '', loadChildren: () => import('./clientes-list/clientes-list.module').then(m => m.ClientesListModule) },
-      { path: 'listar', loadChildren: () => import('./clientes-list/clientes-list.module').then(m => m.ClientesListModule)},
-      { path: 'nuevo', loadChildren: () => import('./clientes-form/clientes-form.module').then(m => m.ClientesFormModule) },
-      { path: ':id', loadChildren: () => import('./clientes-detalle/clientes-detalle.module').then(m => m.ClientesDetalleModule) },
-      { path: 'editar/:id', loadChildren: () => import('./clientes-form/clientes-form.module').then(m => m.ClientesFormModule) }
+      { path: 'listar', loadChildren: () => import('./clientes-list/clientes-list.module').then(m => m.ClientesListModule)}
     ]
   }
 ];
