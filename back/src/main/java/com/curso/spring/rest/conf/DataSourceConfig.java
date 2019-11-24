314f6e9ac36ba61ff54623e3d20569e83d922808
Jimmyfk <jjsaesd01@gmail.com>
2019-11-24 19:54:22 +0100

    Datasource configurado

--- back/src/main/java/com/curso/spring/rest/conf/DataSourceConfig.java
+++ back/src/main/java/com/curso/spring/rest/conf/DataSourceConfig.java
@@ -8,15 +8,29 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
 
 @PropertySource(value = "classpath:datasource.properties")
 @Configuration
+@EnableTransactionManagement
 public class DataSourceConfig {
 
     private static final Logger LOG = LoggerFactory.getLogger(DataSourceConfig.class);
@@ -39,6 +53,18 @@ public class DataSourceConfig {
     @Value(value = "${ruta.script.inicial}")
     private String rutaScript;
 
+    @Value(value = "${db.dialect}")
+    private String dialecto;
+
+    @Value(value = "${db.ddl-auto}")
+    private String ddlAuto;
+
+    @Value(value = "${db.show-sql}")
+    private String showSql;
+
+    @Value(value = "${db.new-id-generator}")
+    private Boolean newIdGenerator;
+
     @Value(value = "${db.esquema.creado}")
     private Boolean creado;
 
@@ -54,9 +80,42 @@ public class DataSourceConfig {
         if (!creado) {
             crearEsquema(dataSource);
         }
+
+        comprobarCreacion(dataSource);
         dataSource.setSchema(esquema);
 
         return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(dataSource());
+        entityManager.setPackagesToScan("com.curso.spring.rest.model");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        entityManager.setJpaVendorAdapter(vendorAdapter);
+        entityManager.setJpaProperties(configuracionesAdicionales());
+
+        return entityManager;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
     private void crearEsquema(DriverManagerDataSource dataSource) {
@@ -79,5 +138,30 @@ public class DataSourceConfig {
             e.printStackTrace();
             LOG.error("Error al cambiar la configuración del fichero datasource.properties");
         }
+        this.creado = creado;
     }
+
+    private Properties configuracionesAdicionales() {
+        Properties properties = new Properties();
+
+        properties.setProperty("hibernate.dialect", dialecto);
+        properties.setProperty("hibernate.ddl-auto", ddlAuto);
+        properties.setProperty("show-sql", showSql);
+        properties.setProperty("hibernate.use-new-id-generator-mappings", newIdGenerator.toString());
+
+        return properties;
+    }
+
+    private boolean comprobarCreacion(DriverManagerDataSource dataSource) {
+        try {
+            Connection connection = dataSource.getConnection();
+            connection.getCatalog();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            setEsquemaCreado(false);
+            return false;
+        }
+    }
+
 }
